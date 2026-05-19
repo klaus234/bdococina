8c210878d1097da66e42b5739717dbaed88c4b2b
Klaus <klausmaster@gmail.com>
2026-05-19 02:27:38 -0300

    fix merge cases

--- bdococina.js
+++ bdococina.js
@@ -351,12 +351,14 @@ function modificadorIngrediente(e) {
     const imperiales = document.getElementById("imperiales");
     const imperiales_especiales = document.getElementById("imperiales_especiales");
     const imperiales_total = document.getElementById("imperiales_total");
-    if(imperiales != undefined)
+    if(imperiales != undefined) {
         imperiales.value = Math.floor(total.value / imperiales.multiplicador);
         imperiales_especiales.value = Math.floor(total_especiales.value / imperiales_especiales.multiplicador);
         imperiales_total.value = Math.floor(parseInt(imperiales.value) + parseInt(imperiales_especiales.value));
+        actualizarDiasUI();
+    }
 }
-let noModFlagDia = false;
+
 function modificarSegunCantidad() {
     actualizarIngredientes(this.value);
     const total = document.getElementById("total");
@@ -364,7 +366,6 @@ function modificarSegunCantidad() {
     const imperiales = document.getElementById("imperiales");
     const imperiales_especiales = document.getElementById("imperiales_especiales");
     const imperiales_total = document.getElementById("imperiales_total");
-    const imperiales_max = document.getElementById("imperiales_max");
     total.value = Math.floor(this.value * document.getElementById("ratio").value);
     if(!flagTotalEspecialesLoad)
         total_especiales.value = Math.floor(this.value * document.getElementById("ratio_especial").value);
@@ -383,9 +384,6 @@ function modificarSegunCantidad() {
         }else{
             flagImperialTotal = false;
         }
-        const dias_imperiales = document.getElementById("dias_imperiales");
-        dias_imperiales.value = Math.floor(imperiales_total.value / imperiales_max.value);
-        
     }
        
 
@@ -404,14 +402,13 @@ function modificarSegunTotal() {
     const imperiales = document.getElementById("imperiales");
     const imperiales_especiales = document.getElementById("imperiales_especiales");
     const imperiales_total = document.getElementById("imperiales_total");
-    const imperiales_max = document.getElementById("imperiales_max");
-
     actualizarIngredientes(cantidadx.value);
     if(imperiales != undefined)
     {
         imperiales.value = Math.floor(this.value / imperiales.multiplicador);
         imperiales_especiales.value = Math.floor(total_especiales.value / imperiales_especiales.multiplicador);
         imperiales_total.value = Math.floor(parseInt(imperiales.value) + parseInt(imperiales_especiales.value));
+        actualizarDiasUI();
     }
        
     updatePeso();
@@ -491,7 +488,6 @@ function modificarSegunDiasImperiales()
     const imperiales_total = document.getElementById("imperiales_total");
 
     imperiales_total.value = dias_imperiales * imperiales_max;
-    noModFlagDia = true;
     const e = new Event("input");
     imperiales_total.dispatchEvent(e);
 }
